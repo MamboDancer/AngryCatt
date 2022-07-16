d1185903c7f41a8459decb3a3cfbc328dd80443b
MamboDancer <mambodosik@gmail.com>
2022-07-16 14:54:53 +0300

    Update Player.cs

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private float forceValue = 1.5f;
+    private float minSpeedToDrag = 0.01f;
+    private Rigidbody2D rb2d;
+    private Camera mainCamera;
+    private Vector2 startPosition;
+    private bool canDrag = true;
+
     void Start()
     {
-        
+      mainCamera = Camera.main;
+      rb2d = GetComponent<Rigidbody2D>();
+      rb2d.isKinematic = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    private void OnMouseDown()
+    {
+      float playerSpeed = rb2d.velocity.magnitude;
+      if(playerSpeed < minSpeedToDrag)
+      {
+        canDrag = true;
+        rb2d.isKinematic = true;
+        rb2d.velocity = Vector2.zero;
+        startPosition = rb2d.position;
+      }
+    }
+
+
+    private void OnMouseUp()
+    {
+      if(canDrag == true)
+      {
+        Vector2 currentPosition = rb2d.position;
+        Vector2 direction = startPosition - currentPosition;
+        rb2d.isKinematic = false;
+        rb2d.AddForce(direction * forceValue);
+        canDrag = false;
+      }
+    }
+
+    private void OnMouseDrag()
+    {
+      if(canDrag)
+      {
+        Vector2 mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector2(mousePosition.x, mousePosition.y);
+      }
+    }
+
+
+
 }
